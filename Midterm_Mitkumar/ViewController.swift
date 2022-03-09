f6abf82f6f72259aab59d8032725aab329490b40
Mit <mitkpatel09@gmail.com>
2022-03-09 18:22:51 -0500

    Initial Commit

---
+++ Midterm_Mitkumar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Midterm_Mitkumar
+//
+//  Created by user215540 on 3/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
